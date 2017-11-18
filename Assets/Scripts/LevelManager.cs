dd95d49c2d22dc6958ea4a00fdfd25ee7d5bde7e
Wojtek-PC\wojciech.stachowiak <stachowiakw@gmail.com>
2017-11-18 18:02:23 +0100

    1st commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LevelChange(string name)
+	{
+		Debug.Log("no elo " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void RequestQuit()
+	{
+		Debug.Log("qqqqqquit!");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+static MusicPlayer Instances = null;
+    void Awake()
+    {
+        Debug.Log("Music player Awake " + GetInstanceID());
+        if (MusicPlayer.Instances != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destruct during awaking");
+        } else {	
+			GameObject.DontDestroyOnLoad(gameObject);
+			Instances = this;
+		}
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
